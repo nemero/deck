7e90a595f2c7f95c9803d30e5100e5c17cca0db2
fmax <datuyo@gmail.com>
2018-08-15 19:09:01 +0200

    D007: implementing main page

--- ui/main.py
+++ ui/main.py
@@ -1,23 +1,76 @@
 import npyscreen
 
-class App(npyscreen.StandardApp):
+class MainUI(npyscreen.NPSAppManaged):
+    """docstring for MainUI"""
     def onStart(self):
-        self.addForm("MAIN", MainForm, name="Welcome to Deck21!!!")
+        self.registerForm("MAIN", MainForm())
+        self.registerForm("CreateServer", CreateServerForm())
+     
 
-class MainForm(npyscreen.ActionForm):
-    """Constructor"""
+class MainForm(npyscreen.Form):
+    """docstring for MainForm"""
     def create(self):
-        self.add(npyscreen.TitleText, name="Welcome to!")
-        self.title = self.add(npyscreen.TitleText, name="TitleText", value="Hello World!", relx=5)
-        #self.add(npyscreen.TitleFilename, name="Filename:", rely=-5)
+        self.name = "Welcome to the Deck21!"
+
+        self.nextrely += 5
+        # Actions
+        self.btn1 = self.add(npyscreen.Button, name="1. Create Session")
+        self.btn2 = self.add(npyscreen.Button, name="2. Join to Session")
+        self.btn3 = self.add(npyscreen.Button, name="3. Exit (Escape)")
+
+        self.online = self.add(npyscreen.FixedText, name="none", value="Online: 5", 
+                                        rely=7, 
+                                        relx=28,
+                                        editable=False
+                                        )
+
+        self.online = self.add(npyscreen.FixedText, name="none", value="Sessions: 1", 
+                                        rely=8, 
+                                        relx=28,
+                                        editable=False
+                                        )
+
+        self.status_bar = self.add(npyscreen.FixedText, name="Note:", value="YOHOHO", 
+                                        rely = -5, 
+                                        relx=3,
+                                        editable=False
+                                        )
+
+    # TODO: add handlers on number hotkeys 1, 2, 3 and Escape
+
+    def afterEditing(self):
+        self.parentApp.setNextForm("MAIN")
+
+    def while_editing(self, widget):
+        #pass
+        self.status_bar.value = "Note: {} has {}".format(widget.name, widget.value)
 
     def on_ok(self):
-        """Event on click Ok"""
-        self.parentApp.setNextForm(None)
-    
-    def on_cancel(self):
-        """Event on click Cancel"""
-        self.title.value = "Hello World!"
+        # Prevent Next Form
+        self.editing = True
 
-MyApp = App()
-MyApp.run()
+    def on_cancel(self):
+        print('form exit')
+        exit()
+
+
+class CreateServerForm(npyscreen.Form):
+    """docstring for MainForm"""
+    def create(self):
+        self.name = "Create Server:"
+        self.myName = self.add(npyscreen.TitleText, name="Name")
+        self.maxPlayers = self.add(npyscreen.TitleText, name="Max Players")
+
+    def afterEditing(self):
+        self.parentApp.setNextForm(None)
+
+
+class EventButton(npyscreen.Button):
+    """docstring for EventButton"""
+    def whenToggled(self):
+        pass
+        
+
+
+if __name__ == '__main__':
+    ui = MainUI().run()
